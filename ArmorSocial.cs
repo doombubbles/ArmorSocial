1cdcfaefd8ccc5c92ab7ee00c1dca46d05c28a24
doombubbles <obsidiantoolzg@gmail.com>
2019-07-11 18:43:15 -0700

    Initial Commit

---
+++ ArmorSocial.cs
@@ -0,0 +1,15 @@
+using Terraria.ModLoader;
+
+namespace ArmorSocial
+{
+	class ArmorSocial : Mod
+	{
+		public ArmorSocial()
+		{
+			Properties = new ModProperties()
+			{
+				Autoload = true
+			};
+		}
+	}
+}
---
+++ ArmorSocialItem.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace ArmorSocial
+{
+    public class ArmorSocialItem : GlobalItem
+    {
+        public override void ModifyTooltips(Item item, List<TooltipLine> tooltips)
+        {
+            if (item.social && (item.headSlot != -1 || item.bodySlot != -1 || item.legSlot != -1))
+            {
+                string secondSetBonus = Main.player[item.owner].GetModPlayer<ArmorSocialPlayer>().secondSetBonus;
+                if (secondSetBonus != "")
+                {
+                    tooltips.Add(new TooltipLine(mod, "SetBonusCheat", "Set Bonus: " + secondSetBonus));
+                }
+            }
+        }
+    }
+}
